c712d11c01a2dbe9ee943dedb67c6971254056e4
NetoPazini <elcio.pazini@hotmail.com>
2024-06-03 11:59:42 -0300

    Adding a simple salary calculator and refactoring flowerbed randomizer for minecraft

---
+++ HowMuchEarnedPerHour.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+_dollarPerHour = 20
+_hoursWorkedByMonth = 140
+caDollarValue = 3.86
+
+def calculateSalaryPerMonth(dollarPerHour, hoursWorkedByMonth):
+	return dollarPerHour * hoursWorkedByMonth
+
+def calculateSalaryPerYear(salaryPerMonth):
+	return salaryPerMonth * 12
+
+salaryPerMonth = calculateSalaryPerMonth(_dollarPerHour, _hoursWorkedByMonth); 
+salaryPerYear = calculateSalaryPerYear(salaryPerMonth);
+
+print(f'Salary per month: {str(salaryPerMonth)} \nSalary per year: {str(salaryPerYear)} \nBRL Month: {str(salaryPerMonth*caDollarValue)} \nBRL Year: {str(salaryPerYear*caDollarValue)}')
--- RandomizeFlowerbedMineCraft.py
+++ RandomizeFlowerbedMineCraft.py
@@ -2,26 +2,31 @@
 import random
 # Here we provide the flower options that we want (in my case, I also inserted my keyboard number related to them)
 flowerNames = ["Cherry sap(1)", "Rose(2)", "Pink bush(3)", "Yellow flower(4)", "White flower(5)", "Lily of the valley (6)", "Blue flower (7)", "Pink flower (8)"]
+flowerNamesLength = len(flowerNames);
 
 # This method return randoms flowers based on the number of tiles in our flowerbed
 # The "repeatFlowers" parameter is used to determine whether repeated flowers should be included or not in the same flowerbed
 def getAllFlowersRandomized(numberOfTiles, repeatFlowers):
 	count = 0
-	selectedFlowers = []
+	selectedFlowersList = []
 
 	for count in range(numberOfTiles):
-		selectedFlower = getSingleRandomFlowerName()
-		while (selectedFlower in selectedFlowers and not repeatFlowers):
-			selectedFlower = getSingleRandomFlowerName()
+		if repeatFlowers or count < flowerNamesLength:
+				selectedFlower = getSingleRandomFlowerName()
+				while selectedFlower in selectedFlowersList and not repeatFlowers:
+					selectedFlower = getSingleRandomFlowerName()
 
-		selectedFlowers.append(selectedFlower)
+				selectedFlowersList.append(selectedFlower)
+		else:
+			print(f"No more unique flowers for the flowerbed \nNumber of unique flowers: {flowerNamesLength} - Number of tiles: {numberOfTiles}")
+			break
 		count = count + 1
 
-	return selectedFlowers
+	return selectedFlowersList
 
 # This method just retrieve a random object of our flowers list
 def getSingleRandomFlowerName():
-	return flowerNames[random.randint(0,7)];
+	return flowerNames[random.randint(0,flowerNamesLength-1)];
 
 
-print(getAllFlowersRandomized(3, False))
+print(getAllFlowersRandomized(10, True))
